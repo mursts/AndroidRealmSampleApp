468b655cbff52413cef971f110056d69957930b3
mursts <mursts@gmail.com>
2016-01-14 01:24:53 +0900

    delete FAB

--- app/src/main/java/jp/mursts/android/androidrealmsampleapp/ItemListActivity.java
+++ app/src/main/java/jp/mursts/android/androidrealmsampleapp/ItemListActivity.java
@@ -5,20 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
 
 import jp.mursts.android.androidrealmsampleapp.dummy.DummyContent;
-
-import java.util.List;
 
 /**
  * An activity representing a list of Items. This activity
@@ -44,15 +40,6 @@ public class ItemListActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         View recyclerView = findViewById(R.id.item_list);
         assert recyclerView != null;
